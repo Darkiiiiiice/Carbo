c6b772170b6bfaad34b44e7f2a81947523109768
MarioMang <853275286@qq.com>
2022-09-20 10:46:00 +0800

    Initial Commit

---
+++ Carbo/CarboApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarboApp.swift
+//  Carbo
+//
+//  Created by MarioMang on 2022/9/20.
+//
+
+import SwiftUI
+
+@main
+struct CarboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
